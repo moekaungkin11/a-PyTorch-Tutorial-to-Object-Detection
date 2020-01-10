14cc5fc3af93010fafb3cc316edb5eb13121f00c
Moe Kaung Kin <41120210+moekaungkin11@users.noreply.github.com>
2020-01-10 16:55:16 +0630

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ from torch import nn
 from utils import *
 import torch.nn.functional as F
 from math import sqrt
-from itertools import product as product
 import torchvision
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
